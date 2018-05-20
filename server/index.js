d6722e65b855c3676b30ecc5d9d185fe4bc1d263
Artem Molokov <molokov.artem@gmail.com>
2018-05-20 22:34:58 +0300

    Serve static files with Express within a Docker container

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const monk = require("monk");
+const webpack = require("webpack");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackConfig = require("../webpack.config.js");
+
+const app = express();
+let db;
+
+function connectToDb() {
+  const url = "mongodb://mongo:27017";
+  db = monk(url);
+  return db.then(() => {
+    console.log("Connected to Mongo");
+  });
+}
+
+const startServer = () => {
+  app.listen("3000", () => {
+    console.log("Started Express server");
+  });
+};
+
+function useWebpackMiddleware() {
+  const developmentCompiler = webpack(webpackConfig);
+  const middleware = webpackMiddleware(developmentCompiler, webpackConfig.devServer);
+  app.use(middleware);
+}
+
+function registerEndpoints() {
+  app.use(express.static("build"));
+}
+
+connectToDb()
+  .then(useWebpackMiddleware)
+  .then(registerEndpoints)
+  .then(startServer);
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "client", "index.js")
+  },
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+
+  devtool: "source-map",
+
+  mode: "development",
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "src"),
+      "node_modules"
+    ]
+  },
+
+  devServer: {
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/
+    },
+    clientLogLevel: "warning",
+    stats: "minimal"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              modules: true,
+              localIdentName: "[local]--[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("postcss-css-reset")(),
+                require("autoprefixer")()
+              ]
+            }
+          },
+          "sass-loader"
+        ],
+        include: [
+          /src/
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "client/index.html"
+    })
+  ]
+};
