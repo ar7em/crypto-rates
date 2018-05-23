02e7dd1edf755a864b86e25b604201c9bdbd88e7
Artem Molokov <artem.molokov@pipedrive.com>
2018-05-23 10:42:12 +0300

    Fix error handler interface

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,7 +7,7 @@ router.use(bodyParser.json());
 
 router.use("/currencies", currencies);
 
-router.use((err, req, res) => {
+router.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
